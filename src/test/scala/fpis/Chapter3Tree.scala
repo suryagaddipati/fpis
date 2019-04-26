baa9a74fe3a2e6e61943469195ae87829f7b289c
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-26 12:45:44 -0500

    Refactor

--- src/test/scala/fpis/Chapter3Tree.scala
+++ src/test/scala/fpis/Chapter3Tree.scala
@@ -175,12 +175,10 @@ class Chapter3Tree extends FunSpec with Matchers {
       assert(zipWith(List(1,2,3),List(4,5,6))(_+_) == List(5,7,9))
     }
     it("3.24"){
-      def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean  = sup match {
-        case Nil => false
-        case Cons(supH, supT) =>  sub match {
-          case Nil => true
-          case Cons(subH, subT) => if(supH == subH) hasSubsequence(supT,subT) else hasSubsequence(supT,sub)
-        }
+      def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean  = (sup,sub) match {
+        case (_,Nil) => true
+        case (Nil,_) => false
+        case (Cons(supH, supT), Cons(subH, subT))=>  if(supH == subH) hasSubsequence(supT,subT) else hasSubsequence(supT,sub)
       }
 
       assert(hasSubsequence(List(1,2,3,4),List(2,3)) == true)
