799555b01b3862e66af9ea5240947684b9537c53
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-28 11:19:23 -0500

    seq

--- src/test/scala/fpis/Chapter4Option.scala
+++ src/test/scala/fpis/Chapter4Option.scala
@@ -43,6 +43,22 @@ class Chapter4Option extends FunSpec with Matchers {
 
     }
 
+    it("4.3"){
+      def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap( x => b.map(y => f(x,y)) )
+      assert(map2(SomeO(1),SomeO(2))(_ + _) == SomeO(3))
+      assert(map2(SomeO(1),None)(_ + _) == None)
+    }
+
+    it("4.4"){
+      def sequence[A](a: List[Option[A]]): Option[List[A]] =
+        a.foldLeft[Option[List[A]]](SomeO(List[A]()))((a,x) => {
+          if(a == None || x == None) None
+          else x.flatMap( k => a.map(j => j :+ k ) )
+        })
+      assert(sequence(List(SomeO(1),SomeO(2))) == SomeO(List(1,2)))
+      assert(sequence(List(SomeO(1),None)) == None)
+    }
+
   }
 
 }
