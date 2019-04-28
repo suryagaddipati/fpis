c31bf02d28df38172d3f83f0b07d831ee92fa155
Surya Gaddipati <surya.gaddipati@gmail.com>
2019-04-28 10:08:11 -0500

    option

--- src/test/scala/fpis/Chapter4Option.scala
+++ src/test/scala/fpis/Chapter4Option.scala
@@ -4,23 +4,32 @@ import org.scalatest._
 
 class Chapter4Option extends FunSpec with Matchers {
 
-  describe("Option") {
-    case class Some[+A](get: A) extends Option[A]
-    case object None extends Option[Nothing]
+  case class SomeO[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
 
-    trait Option[+A] {
-      def map[B](f: A => B): Option[B] = this match {
-        case Some(a) => Some(f(a))
-        case None => None
-      }
-      def flatMap[B](f: A => Option[B]): Option[B] = ???
-      def getOrElse[B >: A](default: => B): B = ???
-      def orElse[B >: A](ob: => Option[B]): Option[B] = ???
-      def filter(f: A => Boolean): Option[A] = ???
+  trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case SomeO(a) => SomeO(f(a))
+      case None => None
     }
 
-    it("4.1"){
+    def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
+    def getOrElse[B >: A](default: => B): B = this match{
+      case SomeO(a) => a
+      case None => default
+    }
+    def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+    def filter(f: A => Boolean): Option[A] = ???
+  }
+
+  describe("Option") {
+
+
+    it("4.1") {
+      assert((SomeO(4)).map(_ * 2) == SomeO(8))
+      assert((SomeO(4)).flatMap(k => SomeO(k * 2)) == SomeO(8))
+      assert((SomeO(4)).getOrElse(0) == 4)
     }
 
   }
